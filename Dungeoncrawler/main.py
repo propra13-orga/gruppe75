8a3ba2278be3add99bfd54e09d40cb9d21bd61bd
Nilkkasukka <Linda.Schaffarczyk@uni-duesseldorf.de>
2013-06-05 14:04:44 +0200

    main.py war noch n fehler drin ich push es nchma wenns korrigiert wurde :D

--- Dungeoncrawler/main.py
+++ Dungeoncrawler/main.py
@@ -142,11 +142,11 @@ def game():
                             pygame.display.flip()
                             time.sleep(3)
                             menu()
-                #        elif next == True:
-                #            next = pygame.image.load(os.path.join(os.path.join("tiles"), "next.png")).convert_alpha()
-                #            screen.blit(fail,(0,0))
-                #            pygame.display.flip()
-                #            time.sleep(3)
+                        #elif level == 4:
+                        #    next = pygame.image.load(os.path.join(os.path.join("tiles"), "level_2.png")).convert_alpha()
+                        #    screen.blit(next, (0,0))
+                        #    pygame.display.flip()
+                        #    time.sleep(3)
                         else:
                             player_pos = (player_pos[0],player_pos[1]-32)
                     elif collision == True:
@@ -175,11 +175,11 @@ def game():
                             pygame.display.flip()
                             time.sleep(3)
                             menu()
-                        elif level == 4:
-                            next = pygame.image.load(os.path.join(os.path.join("tiles"), "level_"+str(level)+".png")).convert_alpha()
-                            screen.blit(next, (0,0))
-                            pygame.display.flip()
-                            time.sleep(3)
+                       # elif level == 4:
+                       #     next = pygame.image.load(os.path.join(os.path.join("tiles"), "level_2.png")).convert_alpha()
+                       #     screen.blit(next, (0,0))
+                       #     pygame.display.flip()
+                        #    time.sleep(3)
                         else:
                             player_pos = (player_pos[0],player_pos[1]+32)
                     elif collision == True:
@@ -208,11 +208,11 @@ def game():
                             pygame.display.flip()
                             time.sleep(3)
                             menu()
-                        elif level == 4:
-                            next = pygame.image.load(os.path.join(os.path.join("tiles"), "level_"+str(level)+".png")).convert_alpha()
-                            screen.blit(next, (0,0))
-                            pygame.display.flip()
-                            time.sleep(3)
+                        #elif level == 4:
+                        #    next = pygame.image.load(os.path.join(os.path.join("tiles"), "level_2.png")).convert_alpha()
+                        #    screen.blit(next, (0,0))
+                        #    pygame.display.flip()
+                        #    time.sleep(3)
                         else:
                             player_pos = (player_pos[0]-32,player_pos[1])
                     elif collision == True:
