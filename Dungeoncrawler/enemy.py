020428aa7bf2c65b7b94b4f53da7c6e27be1d7cc
Ullrich Buschmann <ullrich.buschmann@hhu.de>
2013-06-12 14:45:08 +0200

    ein paar neue methoden
    Signed-off-by: Ullrich Buschmann <ullrich.buschmann@hhu.de>

--- Dungeoncrawler/enemy.py
+++ Dungeoncrawler/enemy.py
@@ -1,11 +1,23 @@
 class Enemy(object):
     def __init__(self, name, weapon, health, mana, armor, image):
-        self.image
+        self.image = image
         self.name = name
         self.weapon = weapon
         self.health = health
         self.mana = mana
         self.armor = armor
+    def get_image(self):
+        return self.image
+    def get_name(self):
+        return self.name
+    def get_weapon(self):
+        return self.weapon
+    def get_health(self):
+        return self.health
+    def get_mana(self):
+        return self.mana
+    def get_armor(self):
+        return self.armor
     def set_mana(self, new_mana):
         self.mana = new_mana
     def set_health(self, new_health):
--- Dungeoncrawler/item.py
+++ Dungeoncrawler/item.py
@@ -1,16 +1,19 @@
 class item(object):
     def __init__(self, name):
         self.name = name
-        
+    def get_name(self):
+        return self.name
         
 class weapon(item):
     def __init__(self, name, damage, range):
         item.__init__(self, name)
         self.damage = damage
         self.range = range
+    def get_damage(self):
+        return self.damage
 
 class armor(item):
     def __init__(self, name, damage_reduction, image):
+        item.__init__(self, name)
         self.image = image
-        self.name = name
         self.damage_reduction = damage_reduction
