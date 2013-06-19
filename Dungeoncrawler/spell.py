3ef824fbb62609368518f1571676ea3f98c43e97
Ullrich Buschmann <ullrich.buschmann@hhu.de>
2013-06-19 11:50:21 +0200

    Signed-off-by: Ullrich Buschmann <ullrich.buschmann@hhu.de>

--- Dungeoncrawler/spell.py
+++ Dungeoncrawler/spell.py
@@ -1,6 +1,12 @@
 import pygame
 import os
 
+def check_for_enemy( player1, enemy1, boss, position):
+    liste = [player1.get_position(),enemy1.get_position(),boss.get_position()]
+    if position in liste:
+        return liste
+    
+
 def check_for_collision(player_pos,map):
     solid_list = map.list_solid_tiles()
     if player_pos in solid_list:
@@ -26,9 +32,10 @@ class spell(object):
         return self.image
     def set_position(self, new_position):
         self.position = new_position
-    def move(self, map):
+    def move(self, map, player1, enemy1, boss):
         if self.direction == "UP":
             solid = check_for_collision((self.position[0],self.position[1]-32),map)
+            enemy = check_for_enemy(playe1, enemy1, boss, (self.position[0],self.position[1]-32))
             if solid == False:
                 self.position = (self.position[0],self.position[1]-32)
                 return True
@@ -36,6 +43,7 @@ class spell(object):
                 return False
         elif self.direction == "DOWN":
             solid = check_for_collision((self.position[0],self.position[1]+32),map)
+            enemy = check_for_enemy(playe1, enemy1, boss, (self.position[0],self.position[1]+32))
             if solid == False:
                 self.position = (self.position[0],self.position[1]+32)
                 return True
@@ -43,6 +51,7 @@ class spell(object):
                 return False
         elif self.direction == "LEFT":
             solid = check_for_collision((self.position[0]-32,self.position[1]),map)
+            enemy = check_for_enemy(playe1, enemy1, boss, (self.position[0]-32,self.position[1]))
             if solid == False:
                 self.position = (self.position[0]-32,self.position[1])
                 return True
@@ -50,6 +59,7 @@ class spell(object):
                 return False
         elif self.direction == "RIGHT":
             solid = check_for_collision((self.position[0]+32,self.position[1]),map)
+            enemy = check_for_enemy(playe1, enemy1, boss, (self.position[0]+32,self.position[1]))
             if solid == False:
                 self.position = (self.position[0]+32,self.position[1])
                 return True
